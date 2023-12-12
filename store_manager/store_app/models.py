af75cd6aa341d98df68b1b123734cffee649eb5f
G850329 <146905662+G850329@users.noreply.github.com>
2023-12-12 11:29:34 -0300

    Update models.py

--- store_manager/store_app/models.py
+++ store_manager/store_app/models.py
@@ -9,16 +9,16 @@ class Store(Model):
 
     """
 
-   
-   nombre = models.CharField(max_length=100)
-    estilo = models.CharField(max_length=50)
-    alcohol_porcentaje = models.FloatField(default=0.0)
-    volumen = models.FloatField(default=0.0)
-    precio = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
-    cantidad_disponible = models.IntegerField(default=0)
-    
-    ubicacion = models.CharField(max_length=150, blank=True, null=True)
-    fecha_habilitacion = models.DateField(auto_now=True)
+nombre = models.CharField(max_length=100)
+estilo = models.CharField(max_length=50)
+alcohol_porcentaje = models.FloatField(default=0.0)
+volumen = models.FloatField(default=0.0)
+precio = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
+cantidad_disponible = models.IntegerField(default=0)
+
+ubicacion = models.CharField(max_length=150, blank=True, null=True)
+fecha_habilitacion = models.DateField(auto_now=True)
+
 
     # podemos crear la tabla con un nombre especifico pero se lo tenemos
     # que indicar directamente en la metaclase
